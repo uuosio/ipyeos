73acece16b479563c5d1a365da1e1921d87c682f
learnforpractice <learnforpractice@gmail.com>
2022-01-04 00:33:09 +0800

    Update workflow files

--- scripts/download.py
+++ scripts/download.py
@@ -9,10 +9,10 @@ files = [
     f'ipyeos-{version}-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
     f'ipyeos-{version}-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
     f'ipyeos-{version}-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
-    f'ipyeos-{version}-cp39-cp39-macosx_10_15_x86_64.whl',
-    f'ipyeos-{version}-cp310-cp310-macosx_10_15_x86_64.whl',
-    f'ipyeos-{version}-cp37-cp37m-macosx_10_15_x86_64.whl',
-    f'ipyeos-{version}-cp38-cp38-macosx_10_15_x86_64.whl',
+    f'ipyeos-{version}-cp39-cp39-macosx_10_16_x86_64.whl',
+    f'ipyeos-{version}-cp310-cp310-macosx_10_16_x86_64.whl',
+    f'ipyeos-{version}-cp37-cp37m-macosx_10_16_x86_64.whl',
+    f'ipyeos-{version}-cp38-cp38-macosx_10_16_x86_64.whl',
 ]
 
 url = f'https://github.com/uuosio/pyeos/archive/refs/tags/v{version}.tar.gz'
