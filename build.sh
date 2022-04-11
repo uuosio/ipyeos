3a14e34b20a80a2ba231bb6f33ef666ad0f03f5e
learnforpractice <learnforpractice@gmail.com>
2022-04-11 20:30:02 +0800

    build with `--plat-name macosx-10.16-x86_64`

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ./release.sh || exit 1
-python3.9 setup.py sdist bdist_wheel
+python3.9 setup.py sdist bdist_wheel --plat-name macosx-10.16-x86_64
 if [ $? -eq 0 ]; then
 ./install.sh
 fi
--- scripts/download.py
+++ scripts/download.py
@@ -5,10 +5,10 @@ import subprocess
 
 version = sys.argv[1]
 files = [
-    f'ipyeos-{version}-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
-    f'ipyeos-{version}-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
-    f'ipyeos-{version}-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
-    f'ipyeos-{version}-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
+    # f'ipyeos-{version}-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
+    # f'ipyeos-{version}-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
+    # f'ipyeos-{version}-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
+    # f'ipyeos-{version}-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl',
     f'ipyeos-{version}-cp39-cp39-macosx_10_16_x86_64.whl',
     f'ipyeos-{version}-cp310-cp310-macosx_10_16_x86_64.whl',
     f'ipyeos-{version}-cp37-cp37m-macosx_10_16_x86_64.whl',
