c7b45530eafcb33c1b4fd77f15268fc9eb5207a4
learnforpractice <learnforpractice@gmail.com>
2022-08-15 21:00:48 +0800

    Fix undefined symbol: get_vm_api_proxy on the Linux platform

--- tag.sh
+++ tag.sh
@@ -1,6 +1,6 @@
-VERSION=v0.2.6
+VERSION=v0.2.7
 TARGET=origin
-# git push $TARGET :refs/tags/$VERSION
+git push $TARGET :refs/tags/$VERSION
 git tag -d $VERSION
 git tag $VERSION -F release.txt
 git push -f $TARGET $VERSION
