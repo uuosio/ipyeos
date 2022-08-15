ef444da685e0c8d379e8fe289b577b0e96c3fcae
learnforpractice <learnforpractice@gmail.com>
2022-08-15 16:03:55 +0800

    Bump version to v0.2.6

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ release_files.extend([
 
 setup(
     name="ipyeos",
-    version="0.2.5",
+    version="0.2.6",
     description="IPYEOS project",
     author='The IPYEOS Team',
     license="MIT",
