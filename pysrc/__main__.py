278a73d45f91985636f9e26e0bb9ed71c2051fcb
learnforpractice <learnforpractice@gmail.com>
2022-08-16 00:47:47 +0800

    Improve main function

--- pysrc/__main__.py
+++ pysrc/__main__.py
@@ -6,22 +6,27 @@ from ipyeos import run
 INIT_SUCCESS = 6
 
 def main():
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest='subparser')
+
+    debug_sub_parser = subparser.add_parser('eos-debugger', help='start eos debugger server')
+    debug_sub_parser.add_argument('--addr', default="127.0.0.1", help="eos debugger server address")
+    debug_sub_parser.add_argument('--server-port', default="9090", help="eos debugger server port")
+    debug_sub_parser.add_argument('--vm-api-port', default="9092", help="eos debugger vm api port")
+    debug_sub_parser.add_argument('--apply-request-addr', default="127.0.0.1", help="client side apply request server address")
+    debug_sub_parser.add_argument('--apply-request-port', default="9091", help="client side apply request server port")
+    debug_sub_parser.add_argument('--rpc-server-addr', default="127.0.0.1", help="rpc server address")
+    debug_sub_parser.add_argument('--rpc-server-port', default="9093", help="rpc server port")
+
+    start_eos_parser = subparser.add_parser('eos-node', help='run a eos node')
+
+    if len(sys.argv) <= 1:
+        parser.print_help()
+        sys.exit(-1)
+
     if 'RUN_IPYEOS' in os.environ:
         from ipyeos import eos
         from ipyeos import server
-        parser = argparse.ArgumentParser()
-        subparser = parser.add_subparsers(dest='subparser')
-
-        debug_sub_parser = subparser.add_parser('eos-debugger', help='start eos debugger server')
-        debug_sub_parser.add_argument('--addr', default="127.0.0.1", help="eos debugger server address")
-        debug_sub_parser.add_argument('--server-port', default="9090", help="eos debugger server port")
-        debug_sub_parser.add_argument('--vm-api-port', default="9092", help="eos debugger vm api port")
-        debug_sub_parser.add_argument('--apply-request-addr', default="127.0.0.1", help="client side apply request server address")
-        debug_sub_parser.add_argument('--apply-request-port', default="9091", help="client side apply request server port")
-        debug_sub_parser.add_argument('--rpc-server-addr', default="127.0.0.1", help="rpc server address")
-        debug_sub_parser.add_argument('--rpc-server-port', default="9093", help="rpc server port")
-
-        start_eos_parser = subparser.add_parser('eos-node', help='run a eos node')
 
         if sys.argv[1] == 'eos-node':
             argv = sys.argv[1:]
@@ -31,16 +36,18 @@ def main():
                 sys.exit(ret)
             eos.exec()
             print('done!')
-        else:
+        elif sys.argv[1] == 'eos-debugger':
             result, unknown = parser.parse_known_args()
-            if result.subparser == 'eos-debugger':
-                server.start_debug_server(result.addr, result.server_port, result.vm_api_port, result.apply_request_addr, result.apply_request_port, result.rpc_server_addr,result.rpc_server_port)
-            else:
-                parser.print_help()
+            server.start_debug_server(result.addr, result.server_port, result.vm_api_port, result.apply_request_addr, result.apply_request_port, result.rpc_server_addr,result.rpc_server_port)                
+        else:
+            parser.print_help()
     else:
-        custom_cmds = ['-m', 'ipyeos']
-        custom_cmds.extend(sys.argv[1:])
-        run.run_ipyeos(custom_cmds)
+        if sys.argv[1] in ['eos-node', 'eos-debugger']:
+            custom_cmds = ['-m', 'ipyeos']
+            custom_cmds.extend(sys.argv[1:])
+            run.run_ipyeos(custom_cmds)
+        else:
+            run.run_ipyeos()
 
 if __name__ == "__main__":
     main()
